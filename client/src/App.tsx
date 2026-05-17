50faa09270a5760115d8cc38acd80a1dd50de068
felix chen <felixchen3674@gmail.com>
2026-05-17 17:15:05 -0400

    syntax

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import { MantineProvider } from '@mantine/core';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider } from 'react-router-dom';
+import { MantineProvider } from "@mantine/core";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
 
-import { AuthProvider } from './features/auth/AuthContext';
-import { queryClient } from './lib/queryClient';
-import { router } from './router';
+import { AuthProvider } from "./features/auth/AuthContext";
+import { queryClient } from "./lib/queryClient";
+import { router } from "./router";
 
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 
 function App() {
   return (
