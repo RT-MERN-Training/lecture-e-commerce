601268b2fb88a4adf0013ec4078812c10547412f
felix chen <felixchen3674@gmail.com>
2026-05-18 02:55:48 -0400

    test

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -23,41 +23,38 @@ const withBoundary = (name: string, children: ReactNode) => (
   </RouteErrorBoundary>
 );
 
-export const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: <RootLayout />,
-      errorElement: <GlobalErrorPage />,
-      children: [
-        { index: true, element: withBoundary("Home", <Home />) },
-        { path: "products", element: withBoundary("Products", <Products />) },
-        {
-          path: "products/:id",
-          element: withBoundary("ProductDetail", <ProductDetail />),
-        },
-        { path: "login", element: withBoundary("Login", <Login />) },
-        { path: "signup", element: withBoundary("Signup", <Signup />) },
-        {
-          path: "cart",
-          element: withBoundary(
-            "Cart",
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>,
-          ),
-        },
-        {
-          path: "settings",
-          element: withBoundary(
-            "Settings",
-            <ProtectedRoute>
-              <Settings />
-            </ProtectedRoute>,
-          ),
-        },
-      ],
-    },
-  ],
-  { basename: "/lecture-e-commerce" },
-);
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <GlobalErrorPage />,
+    children: [
+      { index: true, element: withBoundary("Home", <Home />) },
+      { path: "products", element: withBoundary("Products", <Products />) },
+      {
+        path: "products/:id",
+        element: withBoundary("ProductDetail", <ProductDetail />),
+      },
+      { path: "login", element: withBoundary("Login", <Login />) },
+      { path: "signup", element: withBoundary("Signup", <Signup />) },
+      {
+        path: "cart",
+        element: withBoundary(
+          "Cart",
+          <ProtectedRoute>
+            <Cart />
+          </ProtectedRoute>,
+        ),
+      },
+      {
+        path: "settings",
+        element: withBoundary(
+          "Settings",
+          <ProtectedRoute>
+            <Settings />
+          </ProtectedRoute>,
+        ),
+      },
+    ],
+  },
+]);
